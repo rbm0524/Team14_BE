56dd00253ad510be863e79364bf6db6f6c451170
ajy9851 <ajy4304@naver.com>
2024-11-08 17:02:19 +0900

    refactor: 메서드명 변경

--- src/main/java/com/ordertogether/team14_be/order/details/dto/update/UpdateOrderPriceReq.java
+++ src/main/java/com/ordertogether/team14_be/order/details/dto/update/UpdateOrderPriceReq.java
@@ -1,5 +1,3 @@
 package com.ordertogether.team14_be.order.details.dto.update;
 
-public record UpdateOrderPriceReq(Long orderId, int price) {
-
-}
+public record UpdateOrderPriceReq(Long orderId, int price) {}
