b6f4e3053ace469384adff8c832cfbafaa43973d
BOMIN LYU <rbm0524@naver.com>
2024-11-10 12:09:23 +0900

    feat: MemberId 관련 기능 추가

--- src/main/java/com/ordertogether/team14_be/spot/mapper/SpotMapper.java
+++ src/main/java/com/ordertogether/team14_be/spot/mapper/SpotMapper.java
@@ -1,5 +1,7 @@
 package com.ordertogether.team14_be.spot.mapper;
 
+import com.ordertogether.team14_be.member.application.service.MemberService;
+import com.ordertogether.team14_be.member.persistence.entity.Member;
 import com.ordertogether.team14_be.spot.dto.controllerdto.*;
 import com.ordertogether.team14_be.spot.dto.servicedto.SpotDto;
 import com.ordertogether.team14_be.spot.entity.Spot;
@@ -13,13 +15,20 @@ import org.mapstruct.factory.Mappers;
 public interface SpotMapper {
 	SpotMapper INSTANCE = Mappers.getMapper(SpotMapper.class); // 객체 생성해서 INSTANCE에 할당
 
+	@BeanMapping(ignoreByDefault = false)
+	@Mapping(source = "member.id", target = "memberId") // memberId를 member로 매핑
 	SpotDto toDto(Spot spot);
 
+	@BeanMapping(ignoreByDefault = false)
 	@Mapping(target = "category", ignore = true) // category는 무시
 	SpotDto toSpotDto(SpotCreationRequest spotCreationRequest);
 
+	@BeanMapping(ignoreByDefault = false)
+	@Mapping(target = "member", ignore = true)
 	Spot toEntity(SpotDto spotDto);
 
+	@BeanMapping(ignoreByDefault = false)
+	@Mapping(target = "member", ignore = true) // memberId를 member로 매핑
 	Spot toEntity(SpotDto spotDto, @MappingTarget Spot spot); // 생성 또는 수정할 때 사용
 
 	@BeanMapping(ignoreByDefault = false) // 자동 매핑 활성화
@@ -34,16 +43,27 @@ public interface SpotMapper {
 	@Mapping(target = "category", expression = "java(spotDto.getCategory().getStringCategory())")
 	SpotViewedResponse toSpotViewedResponse(SpotDto spotDto);
 
-	SpotModifyRequest toSpotModifyRequest(SpotDto spotDto);
+	@BeanMapping(ignoreByDefault = false) // 자동 매핑 활성화
+	@Mapping(target = "category", expression = "java(spotDto.getCategory().getStringCategory())")
+	SpotModifyResponse toSpotModifyResponse(SpotDto spotDto);
 
+	@BeanMapping(ignoreByDefault = false)
 	@Mapping(target = "category", ignore = true) // category는 무시
 	SpotDto toSpotDto(SpotModifyRequest spotModifyRequest);
 
 	@AfterMapping
-	default void mapToCategory(
-			SpotCreationRequest spotCreationRequest, @MappingTarget SpotDto spotDto) {
+	default void map(SpotCreationRequest spotCreationRequest, @MappingTarget SpotDto spotDto) {
 		spotDto.setCategory(
 				Category.fromStringToEnum(spotCreationRequest.category())
 						.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 카테고리입니다.")));
+	}
+
+	// Long (memberId) -> Member 객체로 변환
+	@AfterMapping
+	default Member map(Long memberId, MemberService memberService) {
+		if (memberId == null) {
+			return null;
+		}
+		return memberService.findMember(memberId);
 	}
 }
