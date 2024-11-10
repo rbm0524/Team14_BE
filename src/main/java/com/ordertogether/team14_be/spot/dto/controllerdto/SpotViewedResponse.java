30e8171b867b48550af3b1699df855bafc33a1ba
BOMIN LYU <rbm0524@naver.com>
2024-11-10 13:42:20 +0900

    refactor: 위도, 경도도 반환하도록 수정

--- src/main/java/com/ordertogether/team14_be/spot/dto/controllerdto/SpotViewedResponse.java
+++ src/main/java/com/ordertogether/team14_be/spot/dto/controllerdto/SpotViewedResponse.java
@@ -1,9 +1,12 @@
 package com.ordertogether.team14_be.spot.dto.controllerdto;
 
+import java.math.BigDecimal;
 import java.time.LocalTime;
 
 public record SpotViewedResponse(
 		Long id,
+		BigDecimal lat,
+		BigDecimal lng,
 		String category,
 		String storeName,
 		Integer minimumOrderAmount,
