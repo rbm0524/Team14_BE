f65461a0f87880b741bbcda84e03089dba29c262
rbm0524 <rbm0524@naver.com>
2024-11-11 23:03:21 +0900

    feat: Spot에 대한 임시 데이터 넣기

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -24,3 +24,15 @@ INSERT INTO payment_order (id, product_id, order_id, order_name, amount, created
 INSERT INTO payment_event (id, buyer_id, created_at, modified_at, order_id, order_name, payment_key, payment_status) VALUES (5, 2, now(), now(), 'test-order-id-5', 'Product 1, Product 2', 'test-payment-key-5', 'SUCCESS');
 INSERT INTO payment_order (id, product_id, order_id, order_name, amount, created_at, modified_at) VALUES (9, 1, 'test-order-id-5', 'Product 1', 10000, now(), now());
 INSERT INTO payment_order (id, product_id, order_id, order_name, amount, created_at, modified_at) VALUES (10, 2, 'test-order-id-5', 'Product 2', 20000, now(), now());
+
+INSERT INTO spot (master_id, lat, lng, category, store_name, minimum_order_amount, together_order_link, pick_up_location, delivery_status, deadline_time, geo_hash, is_deleted, created_at, modified_at, created_by, modified_by)
+VALUES (1, 35.1766787, 126.9054188, '001', '떡볶이 천국', 10000, 'https://example.com/order/1', '정문 앞', 'OPEN', '21:30:00', 'wy60pg9xn84v', false, now(), now(), 1, 2);
+
+INSERT INTO spot (master_id, lat, lng, category, store_name, minimum_order_amount, together_order_link, pick_up_location, delivery_status, deadline_time, geo_hash, is_deleted, created_at, modified_at, created_by, modified_by)
+VALUES (2, 35.1766787, 126.9054188, '001', '김밥의 집', 8000, 'https://example.com/order/2', '후문 앞', 'CLOSED', '20:00:00', 'wy60pg9xn84v', false, now(), now(), 2, 3);
+
+INSERT INTO spot (master_id, lat, lng, category, store_name, minimum_order_amount, together_order_link, pick_up_location, delivery_status, deadline_time, geo_hash, is_deleted, created_at, modified_at, created_by, modified_by)
+VALUES (1, 35.1766787, 126.9054188, '001', '순대나라', 5000, 'https://example.com/order/3', '주차장', 'OPEN', '22:00:00', 'wy60pg9xn84v', true, now(), now(), 3, 1);
+
+INSERT INTO spot (master_id, lat, lng, category, store_name, minimum_order_amount, together_order_link, pick_up_location, delivery_status, deadline_time, geo_hash, is_deleted, created_at, modified_at, created_by, modified_by)
+VALUES (2, 35.1766787, 126.9054188, '001', '튀김천국', 12000, 'https://example.com/order/4', '엘리베이터 앞', 'OPEN', '23:00:00', 'wy60pg9xn84v', false, now(), now(), 4, 5);
