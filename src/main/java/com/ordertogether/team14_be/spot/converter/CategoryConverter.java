a31136653370023bf2238c707b841915619b894c
BOMIN LYU <rbm0524@naver.com>
2024-11-15 06:44:17 +0900

    fix: Converter 수정

--- src/main/java/com/ordertogether/team14_be/spot/converter/CategoryConverter.java
+++ src/main/java/com/ordertogether/team14_be/spot/converter/CategoryConverter.java
@@ -3,7 +3,7 @@ package com.ordertogether.team14_be.spot.converter;
 import com.ordertogether.team14_be.spot.enums.Category;
 import jakarta.persistence.Converter;
 
-@Converter(autoApply = true)
+@Converter
 public class CategoryConverter extends AbstractCodedEnumConverter<Category, String> {
 
 	public CategoryConverter() {
