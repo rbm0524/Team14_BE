1c79c6c39759209512b8571d1d618070aa66e859
BOMIN LYU <rbm0524@naver.com>
2024-11-21 18:54:42 +0900

    fix: RestController로 수정

--- src/main/java/com/ordertogether/team14_be/auth/presentation/AuthController.java
+++ src/main/java/com/ordertogether/team14_be/auth/presentation/AuthController.java
@@ -16,10 +16,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/auth")
 @Slf4j
 public class AuthController {
